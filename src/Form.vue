033c466e749f920b6b7010b68be893942897dec7
Kotaro <swdevstudio@gmail.com>
2020-06-02 16:58:32 +0300

    Настроили флобитс начали работу с компонентами

---
+++ src/Form.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="form">
+    <card-view/>
+
+  </div>
+
+</template>
+
+<script>
+  import cardView from "./components/cardView";
+
+
+export default {
+  name: 'new',
+  components: {
+    cardView
+
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/cardView.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="card-view">
+        <div class="card-view__image">
+            <img :src="test2" alt="">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "cardView",
+        data(){
+            return {
+                test2: require('../assets/karta.png.png')
+            }
+        },
+        methods: {
+            tests(){
+                return '../assets/karta.png'
+            }
+        },
+
+    }
+</script>
+
+<style scoped lang="scss">
+    .card-view {
+        width: 320px;
+        height: 480px;
+        background: black;
+    }
+</style>
